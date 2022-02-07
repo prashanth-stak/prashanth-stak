4d23324818ad673cf26db21dcae73fb11f1165b6
prashanth-stak <99196742+prashanth-stak@users.noreply.github.com>
2022-02-07 21:02:40 +0530

    adding python files

---
+++ my_practice_print_statements.py
@@ -0,0 +1,80 @@
+"""print("hard work")
+print("123456789")"""
+# introduction to mecanical engineering
+"""print("mechanical engineering")
+print("syllabus of mechanical engineering")"""
+
+"""print("prashanth is from srinivaspur")   #introduction to prashanth
+print ("prashanth completed mtech in ubdt college of engineering davangere")"""
+
+"""print("prashanth did project on design and analysis of rack and pinion scissor arm lift")
+print("ide")      #introduction to integrated development environment software
+print("what is ide")
+print("ide is software for building applications that combines common developer tools into a single gui")
+print("why do we use ide")
+print("6ide's can be used to create software applications")
+print("drivers and utilities")
+print("it allows allows developing software in any programming language without spending much time on language syntax")
+print("ide has the ability to correct syntaxes")
+print("gives a warning about memory leaks, assist in writing quality of code")"""
+
+"""print("a+b")
+print(1234567890)"""
+
+"""print(12+13+14+15+16)
+print(45*8)
+print(456/6)
+print(12*45+58/7-67)
+print(99*8-67*57+98/6)
+print(8888-234+567/9+56*7)"""
+
+"""x=6
+y=89
+z=x*y
+print(z)"""
+
+"""x=900
+y=789
+z=67
+n=x+y-z+x/y
+print(n)"""
+"""a=5
+b=6
+c=7
+d=9
+x=a+b
+y=b+c
+z=c+d
+p=x*y*z-y*z+y*x
+print(p)"""
+
+"""print(2+3)
+print(88+99)
+print(123456789+987654321)
+print(12+34-20+56*78/5+67*787)"""
+print(102*8+9-10*13/2*10)
+
+"""print(2**5)
+print(10//3)
+print(10%3)"""
+"prashu"
+"""print("prashu")
+print("prashu's laptop")
+print('prashu "laptop"')
+print('prashu\'s "laptop"')
+print('prashu+prashu')
+print(10*'prashu')
+print(100*'prashu prashu')
+print('c:\docs\prashu')
+print(r'c:\docs\prashu')"""
+
+"""x = 2
+y = 3
+z = x+y
+print(z)"""
+
+"""print("name='prashanth'")
+print("name+'rocks'")
+print('length("name")')"""
+nums = [23,12,36,44]
+nums[4]
---
+++ practice_print_statements.py
@@ -0,0 +1,35 @@
+# single commenting is done by using (#)
+
+"""Multiple line commenting
+is done in this method"""
+
+# Introduction to print statement
+print("Introduction to python")
+print(7)
+
+# Introduction to variables(constant)
+a = 1         # assigned 1(integer) to variable a
+print(a)
+print("Value of a is ", a)       # printing variable a
+print(type(a))
+b = "Hello world"       # assigned Hello world(string) to variable b
+print("value of b: ", b)                # printing variable b
+print("Type of b is ",type(b))
+
+# Another type of printing
+c = 1
+
+d = "Hello world"
+
+print("value of c is {} and value of d: {}".format(c, d))
+
+print("type of c is {} and type of d: {}".format(type(c), type(d)))
+
+val = "1"
+
+print("value of c is {}, value of d is {} and value of val is {}".format(c, d, val))
+
+# Changing of existing values of variables
+
+a = 2
+print("changed value of a:", a)
